ef7c7cec2d29424fb1ee4fdd26af34ff43ab09b6
John Hagens <ginuwine104@gmail.com>
2023-01-30 16:21:26 -0500

    add notes

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(pubDir, "notes.html"));
   });
 
+  app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+
+  
+
 app.listen(port, function () {
     console.log(`Server listening on port ${port}. At your service!`);
   });
