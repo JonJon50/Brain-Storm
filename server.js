544974aeaeb7af6a88167a75b1f10fb459db7f76
John Hagens <ginuwine104@gmail.com>
2023-01-30 16:28:34 -0500

    remove saved from notes

--- server.js
+++ server.js
@@ -10,28 +10,32 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // get route /notes
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(pubDir, "notes.html"));
-  });
+  res.sendFile(path.join(pubDir, "notes.html"));
+});
 
-  app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-  });
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
-  app.get("/api/notes/:id", function (req, res) {
-    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(notes[Number(req.params.id)]);
-  });
- 
-  app.post("/api/notes", function (req, res) {
-    let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let note = req.body;
-    let id = savedNotes.length.toString();
-    note.id = id;
-    savedNotes.push(note);
+app.get("/api/notes/:id", function (req, res) {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  res.json(notes[Number(req.params.id)]);
+});
+
+app.post("/api/notes", function (req, res) {
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let note = req.body;
+  let id = notes.length.toString();
+  note.id = id;
+  notes.push(note);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  console.log("Note saved to db.json!!");
+  res.json(notes);
+});
 
 app.listen(port, function () {
-    console.log(`Server listening on port ${port}. At your service!`);
-  });
+  console.log(`Server listening on port ${port}. At your service!`);
+});
